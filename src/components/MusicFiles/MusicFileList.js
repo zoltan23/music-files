f912c15aa57216acfe2347000c929440fb42a6dd
stumurry <stu@stumurry.com>
2019-11-01 22:10:01 -0600

    competition

--- src/components/MusicFiles/MusicFileList.js
+++ src/components/MusicFiles/MusicFileList.js
@@ -7,6 +7,7 @@ import Classify from './Classify.js'
 import '../../../src/icons/my-icons-collection/font/flaticon.css'
 
 let musicRef
+
 const deleteItem = (docId, fileLocation) => {
   let confirmDelete = window.confirm("Are you sure you want to delete me?")
   if (confirmDelete === true) {
@@ -29,9 +30,17 @@ const deleteItem = (docId, fileLocation) => {
   }
 }
 
+const AudioComponent = (props) => {
+  console.log("Audio Component Rendered", props)
+  return (<audio controls>
+    <source src={props.file.filePath} type="audio/wav" />
+  </audio>)
+}
+
 const MusicFileList = (props) => {
   console.log("props in list", props)
   const [musicFiles, setMusicFiles] = useState([])
+  console.log('Here are your music files', musicFiles)
   const [authId, setAuthId] = useState('')
 
   useEffect(() => {
@@ -39,7 +48,7 @@ const MusicFileList = (props) => {
     auth.onAuthStateChanged(firebaseUser => {
       if (firebaseUser) {
         setAuthId(firebaseUser.uid)
-        console.log("firebaseUSer", firebaseUser);
+        // console.log("firebaseUSer", firebaseUser);
         const user = auth.currentUser
         musicRef = db.collection('music').doc(user.uid).collection('musicId');
         musicRef.onSnapshot(snapshot => {
@@ -48,26 +57,26 @@ const MusicFileList = (props) => {
           snapshot.forEach(doc => {
             p.push(new Promise((resolve, reject) => {
               var docData = doc.data()
-              console.log('docData', docData)
+              // console.log('docData', docData)
               try {
                 var pathReference = storage.ref(docData.filePath);
                 pathReference.getDownloadURL().then(path => {
                   docData.filePath = path
-                  console.log("pathReference", pathReference.location.path)
+                  // console.lxog("pathReference", path)
                   docData.fileLocation = pathReference.location.path
-                  console.log('count', ++count)
+                  // console.log('count', ++count)
                   resolve({ ...docData, id: doc.id })
                 })
               } catch (e) {
-                console.log('error processing url', e)
-                console.log('count', ++count)
+                // console.log('error processing url', e)
+                // console.log('count', ++count)
                 resolve({ ...docData, id: doc.id })
               }
             }))
           })
           Promise.all(p).then(pp => {
-            console.log('setting music files', pp.length)
-            console.log('pp', pp)
+            // console.log('setting music files', pp.length)
+            // console.log('pp', pp)
             setMusicFiles(pp)
           })
         })
@@ -89,7 +98,6 @@ const MusicFileList = (props) => {
 
   return (
     <div className="container">
-
       {listHeader()}
       {musicFiles.map(file => (
         <div className="row h-100 justify-content-center align-items-center" key={file.id} >
@@ -97,12 +105,9 @@ const MusicFileList = (props) => {
           <div className="col-sm-1"><Classify /></div>
           <div className="col-sm-3"><FontAwesomeIcon icon={faMusic} />&nbsp;{file.note}</div>
           <div className="col-sm-4">
-            <audio controls>
-              <source src={file.filePath} type="audio/wav" />
-            </audio>&nbsp; &nbsp;
+            <AudioComponent file={file} />
             <FontAwesomeIcon className="fa-3x align" id="buttonAlert" color="red" icon={faTrashAlt} id={file.id} onClick={e => deleteItem(file.id, file.fileLocation)} />
           </div>
-
         </div>
       ))}
     </div>
